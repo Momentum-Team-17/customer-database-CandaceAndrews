d1931887f99b82f577bb7186ce4f7d90f82155f8
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-02-03 13:37:12 -0500

    added phone, cell and address

--- customer-directory.js
+++ customer-directory.js
@@ -1,21 +1,37 @@
 let customerContainer = document.querySelector("#customer-container");
 
 function buildPersonHTML(listName) {
+    //create div
     let newDivElement = document.createElement("div");
     newDivElement.classList.add("person-block");
+
+    //create header with name
     let nameHeader = document.createElement("h2");
     nameHeader.classList.add("name");
     let textName = document.createTextNode(`${listName.name.first} ${listName.name.last}`);
     nameHeader.appendChild(textName);
     newDivElement.appendChild(nameHeader);
-    // console.log(newDivElement)
+
+    //create phone number
+    let phoneNumber = document.createElement("p");
+    phoneNumber.classList.add("phone-number");
+    let phoneText = document.createTextNode(`Phone: ${listName.phone} Cell: ${listName.cell}`);
+    phoneNumber.appendChild(phoneText);
+    newDivElement.appendChild(phoneNumber);
+
+    //create address
+    let address = document.createElement('h3');
+    address.classList.add("address");
+    addressText = document.createTextNode(`${listName.location.street.number} ${listName.location.street.name} ${listName.location.city} ${listName.location.state} ${listName.location.postcode}`);
+    address.appendChild(addressText);
+    newDivElement.appendChild(address);
+
+    //insert into div
     customerContainer.appendChild(newDivElement);
 }
 
 for (let person of customers) {
     buildPersonHTML(person);
-    // console.log(customers)
-    // console.log(person);
 }
 
 
